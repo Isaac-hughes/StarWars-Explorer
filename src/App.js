4587326ef9e5b24f0d11c71ebb35cf544d3c18b2
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-01-19 17:17:08 +0000

    Started buliding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Films from "./components/films"
+import "./App.css";
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/films">Films</Link>
+            </li>
+            <li>
+              <Link to="/people">People</Link>
+            </li>
+            <li>
+              <Link to="/planets">Planets</Link>
+            </li>
+            <li>
+              <Link to="/species">Species</Link>
+            </li>
+            <li>
+              <Link to="/starships">Starships</Link>
+            </li>
+            <li>
+              <Link to="/vehicles">Vehicles</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        <Switch>
+          <Route path="/films">
+            <FilmsPage />
+          </Route>
+          <Route path="/people">
+            <PeoplePage />
+          </Route>
+          <Route path="/planets">
+            <PlanetsPage />
+          </Route>
+          <Route path="/species">
+            <SpeciesPage />
+          </Route>
+          <Route path="/starships">
+            <StarshipsPage />
+          </Route>
+          <Route path="/vehicles">
+            <VehiclesPage />
+          </Route>
+          
+          <Route path="/">
+            <Page />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+function Page() {
+  return <h2>Home</h2>;
+}
+
+function FilmsPage() {
+  return (
+    <div>
+      <h2>Films</h2>
+      <Films />
     </div>
   );
 }
+
+function PeoplePage() {
+  return <h2>People</h2>;
+}
+
+function PlanetsPage() {
+  return <h2>Planets</h2>;
+}
+
+function SpeciesPage() {
+  return <h2>Species</h2>;
+}
+
+function StarshipsPage() {
+  return <h2>Starships</h2>;
+}
+
+function VehiclesPage() {
+  return <h2>Vehicles</h2>;
+}
+
 
 export default App;
---
+++ src/components/films.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+const Films = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://swapi.dev/api/films/")
+        .then(res => res.json())
+        .then(
+            (result) => {
+            setIsLoaded(true);
+            setItems(result);
+            },
+            (error) => {
+            setIsLoaded(true);
+            setError(error);
+            }
+        )
+    }, [])
+    
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        // add cool loading thing
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                {items.results && items.results.map((item, index) => {
+                return (
+                    <div>
+                        <h1 key={index}>{item.title}</h1>
+                        <div className="content">
+                            <p>Producer: {item.producer}</p>
+                            <p>Release Date: {item.release_date}</p>
+                            <p>Opening Crawl: {item.opening_crawl}</p>
+                            <p>Episode ID: {item.episode_id}</p>
+                        </div>
+                    </div>
+                )})}
+            </div>
+        );
+    }   
+}
+
+export default Films;
